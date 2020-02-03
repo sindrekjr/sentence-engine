84419bfb7544c4691c974ce3776f30a79422dfba
Sindre S. Kjær <sindre@sindrekjr.no>
2020-02-03 22:02:33 +0100

    fix: solved object cloning with lodash in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Sentence = require('./Sentence.js')
+const _ = require('lodash')
 
 /**
  * Constants that keep the initial settings
@@ -15,24 +16,35 @@ const {
  * Variables to store standard settings for the sentence engine
  * The standard settings can be changed by the user
  */
-let standardTemplates = InitialTemplates
-let standardVocabulary = InitialVocabulary
-let standardOptions = InitialOptions
+let standardTemplates = [...InitialTemplates]
+let standardVocabulary = _.cloneDeep(InitialVocabulary)
+let standardOptions = _.cloneDeep(InitialOptions)
 
 /**
  * The function exported as the interface for the package
  */
 function SentenceInterface(template, vocabulary, options) {
+    if(vocabulary && typeof(vocabulary) != 'object') {
+        throw new TypeError('Argument "vocabulary" was expected to be an object.')
+    }
+    if(options && typeof(options) != 'object') {
+        if(typeof(options) != 'object') {
+            throw new TypeError('Argument "options" was expected to be an object.')
+        } else {
+            options = Object.assign(_.cloneDeep(standardOptions), options)
+        }
+    }
+
     return new Sentence(
         template || standardTemplates,
         vocabulary || standardVocabulary,
-        Object.assign(standardOptions, options)
+        options || standardOptions
     )
 }
 
-SentenceInterface.setStandardTemplates = (templates) => standardTemplates = templates || InitialTemplates
-SentenceInterface.setStandardVocab = (vocab) => standardVocabulary = vocab || InitialVocabulary
-SentenceInterface.setStandardOptions = (options) => standardOptions = options || InitialOptions
+SentenceInterface.setStandardTemplates = (templates) => standardTemplates = Array.isArray(templates) ? templates : _.cloneDeep(InitialTemplates)
+SentenceInterface.setStandardVocab = (vocab) => standardVocabulary = vocab || _.cloneDeep(InitialVocabulary)
+SentenceInterface.setStandardOptions = (options) => standardOptions = options ? Object.assign(standardOptions, options) : _.cloneDeep(InitialOptions)
 
 // Export the interface
 module.exports = SentenceInterface
