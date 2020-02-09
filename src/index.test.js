1881aa2782916889f7dfd2f1dca6e09a00e9dbda
Sindre S. Kjær <sindre@sindrekjr.no>
2020-02-09 14:34:17 +0100

    tests: typeerror test drafts

--- src/index.test.js
+++ src/index.test.js
@@ -3,14 +3,21 @@ const Sentence = require('./index.js')
 /**
  * Constants
  */
+const Template = 'This is just {a-adjective} template.'
 const Vocabulary = {
     adjective: ['big', 'long', 'short', 'tall', 'hopeless', 'helpful'],
     verb: ['change', 'surf', 'climb', 'defenestrate', 'optimize']
 }
 
 test('toBeSameSentenceOnTwoGetCalls', () => {
-    let template = 'This is a really {adjective} sentence that I don\'t think should ever {verb}.'
-    let sentence = Sentence(template, Vocabulary)
-
+    let sentence = Sentence(Template, Vocabulary)
     expect(sentence.get()).toBe(sentence.get())
 })
+
+/*test('throwsTypeErrorWhenPassingInvalidVocab', () => {
+    expect(Sentence(Template, [])).toThrow('TypeError')
+})
+
+test('throwsTypeErrorWhenPassingInvalidOptions', () => {
+    expect(Sentence(Template, Vocabulary, [])).toThrow('TypeError')
+})*/
