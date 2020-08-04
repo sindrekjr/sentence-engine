60ea2e93475213d42be16c9167d7b13439634c73
Sindre S. Kjær <sindre@sindrekjr.no>
2020-08-05 00:29:15 +0200

    chore: delete examples folder

--- examples/nonsense/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const Sentence = require('../../src')
-
-const template = '{start} {middle} {end}'
-const components = require('./components.json')
-
-let nonsense = Sentence(template, components)
-console.log(nonsense.get())
