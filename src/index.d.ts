a4ef58c1e13eb7ec63ccb2d831f0133322320a9d
Sindre S. Kjær <sindre@sindrekjr.no>
2020-08-06 01:35:35 +0200

    feat(typescript): declare type Options and extend Array<T> with helper functions

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,34 @@
 declare type Vocabulary = {
   [fieldName: string]: string[];
 }
+
+declare type Options = {
+  allowDuplicates: boolean;
+  capitalize: boolean;
+  forceNewSentence: boolean;
+  placeholderNotation: {
+    start: string;
+    end: string;
+  };
+  preservePlaceholderNotation: boolean;
+}
+
+declare interface Array<T> {
+  any(): T;
+  unique(): T[];
+}
+
+Array.prototype.any = function() {
+  return this[Math.floor(Math.random() * this.length)];
+};
+
+Array.prototype.unique = function() {
+  const a = this.concat();
+  for (let i = 0; i < a.length; ++i) {
+    for (let j = i + 1; j < a.length; ++j) {
+      if (a[i] === a[j]) a.splice(j--, 1);
+    }
+  }
+
+  return a;
+};
