8cba9d6272c103ba46bc9f97313f06039365ae16
Sindre S. Kjær <sindre@sindrekjr.no>
2020-08-16 14:14:36 +0200

    feat(factory): make every method return self-reference for easier chaining

--- src/factory/SentenceFactory.ts
+++ src/factory/SentenceFactory.ts
@@ -18,30 +18,35 @@ export default class SentenceFactory {
     );
   }
 
-  public configure(config: Configuration): void {
+  public configure(config: Configuration): SentenceFactory {
     const { options, templates, vocabulary } = config;
     if (options) this.defaultOptions = { ...this.defaultOptions, ...options };
     if (templates) this.defaultTemplates = templates;
     if (vocabulary) this.defaultOptions = vocabulary;
+    return this;
   }
 
-  public addDefaultOptions(options: MaybeOptions): void {
+  public addDefaultOptions(options: MaybeOptions): SentenceFactory {
     this.defaultOptions = {
       ...this.defaultOptions,
       ...options,
     };
+    return this;
   }
 
-  public addDefaultTemplates(...templates: Template[]): void {
+  public addDefaultTemplates(...templates: Template[]): SentenceFactory {
     this.defaultTemplates = this.defaultTemplates.concat(...templates.flat());
+    return this;
   }
 
-  public addDefaultVocabulary(vocab: Vocabulary): void {
+  public addDefaultVocabulary(vocab: Vocabulary): SentenceFactory {
     this.defaultVocabulary = Object.assign(this.defaultVocabulary, vocab);
+    return this;
   }
 
-  public restoreDefaults(): void {
+  public restoreDefaults(): SentenceFactory {
     this.defaultTemplates = defaults.templates;
     this.defaultVocabulary = defaults.vocabulary;
+    return this;
   }
 }
