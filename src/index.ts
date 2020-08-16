ef55050723d818a0a1024d43869a883a6026cd3a
Sindre S. Kjær <sindre@sindrekjr.no>
2020-08-16 17:05:43 +0200

    fix(entry): wrappers should not return their factory object

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,20 @@ export const createSentence = (
   vocabulary?: Vocabulary,
   options?: MaybeOptions,
 ) => Facade.createSentence(templates, vocabulary, options);
-export const configure = (config: Configuration) => Facade.configure(config);
-export const addDefaultOptions = (options: MaybeOptions) => Facade.addDefaultOptions(options);
-export const addDefaultTemplates = (...templates: Template[]) => Facade.addDefaultTemplates(...templates);
-export const addDefaultVocabulary = (vocab: Vocabulary) => Facade.addDefaultVocabulary(vocab);
-export const restoreDefaults = () => Facade.restoreDefaults();
+export const configure = (config: Configuration) => {
+  Facade.configure(config);
+};
+export const addDefaultOptions = (options: MaybeOptions) => {
+  Facade.addDefaultOptions(options);
+};
+export const addDefaultTemplates = (...templates: Template[]) => {
+  Facade.addDefaultTemplates(...templates);
+};
+export const addDefaultVocabulary = (vocab: Vocabulary) => {
+  Facade.addDefaultVocabulary(vocab);
+};
+export const restoreDefaults = () => {
+  Facade.restoreDefaults();
+};
 
 export { Sentence, SentenceFactory };
